a0a1343c368fe96b659201d26af11cfa715acc46
iLexN <ilex.job@gmail.com>
2024-01-03 06:46:14 +0800

    add commit

--- services/logger/logger.go
+++ services/logger/logger.go
@@ -6,6 +6,10 @@ type Logger interface {
 	Info(msg string)
 }
 
+// The init() function in Go is a special function that is
+// automatically called before the program starts.
+// It is typically used for initialization tasks,
+// such as setting up global variables, configuring packages,
 func init() {
 	//	DefaultLogger = CreateZapLog()
 	DefaultLogger = CreateZeroLog()
