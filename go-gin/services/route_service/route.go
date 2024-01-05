40e16c4813e91f0ba69b66e40b4999ec8beb5df5
iLexN <ilex.job@gmail.com>
2024-01-05 15:00:59 +0800

    add middleware to route

--- go-gin/services/route_service/route.go
+++ go-gin/services/route_service/route.go
@@ -3,6 +3,7 @@ package route_service
 import (
 	"github.com/gin-gonic/gin"
 	controllers "go1/controller"
+	middleware "go1/middleware"
 	"net/http"
 )
 
@@ -13,4 +14,5 @@ func CreateRoute(r *gin.Engine) {
 	})
 
 	r.POST("/login", controllers.LoginHandler)
+	r.GET("/try-check-jwt", middleware.AuthJwt(), controllers.CheckJwt)
 }
