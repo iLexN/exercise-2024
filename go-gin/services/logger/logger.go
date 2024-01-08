91834e6e1a416e34cd3e03148041a4cc45528efc
iLexN <ilex.job@gmail.com>
2024-01-08 11:53:53 +0800

    add comment note

--- go-gin/services/logger/logger.go
+++ go-gin/services/logger/logger.go
@@ -2,6 +2,12 @@ package logger
 
 var DefaultLogger Logger
 
+// should i use other interface
+// some info:
+// https://github.com/go-logr/logr
+// https://www.reddit.com/r/golang/comments/6bptkl/why_isnt_there_a_logger_interface_in_the_standard/?rdt=36955
+// https://github.com/golang/go/issues/28412
+// https://medium.com/@ansujain/building-a-logger-wrapper-in-go-with-support-for-multiple-logging-libraries-48092b826bee
 type Logger interface {
 	Info(msg string)
 	Error(msg string, err error)
