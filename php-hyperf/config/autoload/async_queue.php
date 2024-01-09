98275e015178cdad306564f26b6818b16107ff02
iLexN <ilex.job@gmail.com>
2024-01-09 15:10:58 +0800

    change queue channel name in php-hyperf

--- php-hyperf/config/autoload/async_queue.php
+++ php-hyperf/config/autoload/async_queue.php
@@ -15,7 +15,7 @@ return [
         'redis' => [
             'pool' => 'default',
         ],
-        'channel' => '{queue}',
+        'channel' => 'player', // can change
         'timeout' => 2,
         'retry_seconds' => 5,
         'handle_timeout' => 10,
