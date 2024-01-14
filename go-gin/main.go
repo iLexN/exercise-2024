1241b954b85fbf4edb5f4bdd0f43ca6fac0c9233
iLexN <ilex.job@gmail.com>
2024-01-15 06:20:20 +0800

    move env init to main init, due to the test fail which cannot load the .env

--- go-gin/main.go
+++ go-gin/main.go
@@ -3,7 +3,20 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "go1/cmd"
+import (
+	"github.com/joho/godotenv"
+	"go1/cmd"
+	"go1/services/env"
+	"log"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	env.JwtConfig = env.CreateJwtSetting()
+}
 
 func main() {
 	cmd.Execute()
--- go-gin/services/env/env.go
+++ go-gin/services/env/env.go
@@ -1,17 +1,12 @@
 package env
 
-import (
-	"github.com/joho/godotenv"
-	"log"
-)
-
 // JwtConfig gobal var, may use in defferent file
 var JwtConfig JwtSetting
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	JwtConfig = CreateJwtSetting()
-}
+//func init() {
+//	//	err := godotenv.Load()
+//	//	if err != nil {
+//	//		log.Fatal("Error loading .env file")
+//	//	}
+//	JwtConfig = CreateJwtSetting()
+//}
